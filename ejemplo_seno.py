3a779624aa240de769a6018d651528917786b856
BeatrizZenteno <zentenoq@gmail.com>
2016-09-13 22:43:49 -0300

    Cambio en el plot

--- ejemplo_seno.py
+++ ejemplo_seno.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(0,15)
-plt.plot(x, np.sin(x), 'o-')
-plt.title('Holi')
+plt.plot(x, np.sin(x), '*')
+plt.title('Beatriz c:')
 plt.savefig("Seno")
 plt.show()
